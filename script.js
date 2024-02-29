0c839cf60cc6bd9d424c5b020a5e1c0220b7ebd2
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2024-02-29 19:33:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let array1 = [];
+	let array2 = [];
+	
+	let sum = 0;
+	for (let i=0;i<arr.length;i++){
+	    sum+=arr[i];
+	    if(sum<=n){
+	        array2.push(arr[i]);
+	    }
+	    else{
+	        sum=0;
+	        array1.push(array2);
+	        i=i-1;
+	        array2=[];
+	    }
+	    if(i===arr.length-1){
+	        array1.push(array2);
+	    }
+	}
+	return array1;
 };
 
 const n = prompt("Enter n: ");
